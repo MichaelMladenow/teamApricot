9e23cd874cbca70b1c153d3370ee02d3e9e1c147
Clawfngr <durankievg@gmail.com>
2014-09-14 20:59:15 +0300

    minor changes

--- GameProject/src/InfoPanel.java
+++ GameProject/src/InfoPanel.java
@@ -61,28 +61,28 @@ public class InfoPanel extends JPanel
         g.setColor(Color.black);         
         ImageIcon two=new  ImageIcon("src/2.png");
         Image shiptwo=two.getImage();
-        g.drawImage(shiptwo,115,30,null);
+        g.drawImage(shiptwo,150,30,null);
         g.drawString("Minesweeper (2)",20,40);
         g.setColor(Color.yellow);
         g.fillRect(5,45,10,10);
         g.setColor(Color.black);
         ImageIcon three=new  ImageIcon("src/3.png");
         Image shipthree=three.getImage();
-        g.drawImage(shipthree,110,40,null);
+        g.drawImage(shipthree,150,40,null);
         g.drawString("Frigate (3)",20,55);  
         g.setColor(Color.blue);
         g.fillRect(5,60,10,10);
         g.setColor(Color.black);     
         ImageIcon four=new  ImageIcon("src/4.png");
         Image shipfour=four.getImage();
-        g.drawImage(shipfour,110,55,null);
+        g.drawImage(shipfour,150,55,null);
         g.drawString("Cruiser (4)",20,70); 
         g.setColor(Color.red);        
         g.fillRect(5,75,10,10);
         g.setColor(Color.black);
         ImageIcon five=new  ImageIcon("src/5.png");
         Image shipfive=five.getImage();
-        g.drawImage(shipfive,105,70,null);
+        g.drawImage(shipfive,150,70,null);
         
         g.drawString("Battleship (5)",20,85);        
     }    
